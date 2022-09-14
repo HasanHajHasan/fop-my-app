c41fe8d0cfd605cdf1eac5b1de34d27616d85923
hasanhajhasan <hasanhajhasan98@gmail.com>
2022-09-14 17:47:18 +0300

    updload file to s3

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 const express = require('express')
 const { exec } = require("child_process");
+const AWS = require('aws-sdk')
 
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = 8080
+const port = 8081
 
 app.use(bodyParser.json())
 
-app.get('/health',(req,res)=>{
+app.get('/health', (req, res) => {
     res.header({
-        "System-Health" :true,
+        "System-Health": true,
     })
     res.sendStatus(204)
 })
 
-app.post('/sum',(req,res)=>{
+app.post('/sum',async  (req, res) => {
     const result = req.body.number1 + req.body.number2
     res.status(204).json({
-        result : result// ./fop -fo examples/fo/tables/headfoot.fo -pdf ./header-table.pdf 
+        result: result// ./fop -fo examples/fo/tables/headfoot.fo -pdf ./header-table.pdf 
     })
-    exec("./trialscript 'headfoot.fo' './header-1.pdf'", (error, stdout, stderr) => {
-        console.log("ls--->>")
+
+    exec("./trialscript 'headfoot.fo' '../../opt/fop-my-app/pdfs/header-1.pdf'", (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             return;
@@ -32,9 +33,39 @@ app.post('/sum',(req,res)=>{
         }
         console.log(`stdout: ${stdout}`);
     });
-    console.log("0000>>>>",result)
+    const s3 = new AWS.S3({
+        region:'us-east-1',
+    })
+    const filePdf = './pdfs/header-1.pdf'
+    const uploadImageResult = await fetch(filePdf)
+    const blob = await uploadImageResult.buffer()
+
+    const uploadedImage = await s3.upload({
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: req.files[0].originalFilename,
+        Body: blob,
+    }).promise()
+    console.log(uploadedImage.Location)
+    console.log("0000>>>>", result)
 })
 
-app.listen(port,()=>{
-    console.log('listening to port ',port)
+// app.get('/get-pdf',(req,res)=>{
+
+//     let reporter = new Reporter().use(require('./')())
+
+//     reporter.init()
+//     jsreport.init()
+
+//     const request = {
+//         template: { content: fs.readFileSync(path.join(__dirname, '/hello.fo')).toString(), recipe: 'fop-pdf', engine: 'none' }
+//       }
+//       console.log("--->>>>",request)
+//       return reporter.render(request).then(function (response) {
+//         response.content.toString().should.containEql('%PDF')
+//       })
+
+// })
+
+app.listen(port, () => {
+    console.log('listening to port ', port)
 })

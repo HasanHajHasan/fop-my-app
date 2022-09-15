0b441974514b65e8f118b7f2038b3f657fc0f896
hasanhajhasan <hasanhajhasan98@gmail.com>
2022-09-15 15:45:42 +0300

    set up json to xml

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const { exec } = require("child_process");
 const AWS = require('aws-sdk')
-
+var convert = require('xml-js');
+const fs = require("fs");
 const bodyParser = require('body-parser')
 
 const app = express()
@@ -10,18 +11,22 @@ const port = 8080
 app.use(bodyParser.json())
 
 app.get('/health', (req, res) => {
-    res.header({
-        "System-Health": true,
-    })
+    var options = {compact: true, ignoreComment: true, spaces: 4};
+    var result2 = convert.json2xml(req.body, options);
+    fs.writeFile("fo.xml", result2, function(err, result) {
+        if(err) console.log('error', err);
+      });
+    // fs.unlinkSync("demo.xml");
+    console.log(result2)
     res.sendStatus(204)
+
 })
 
-app.post('/sum',async  (req, res) => {
+app.post('/convert-to-pdf', async (req, res) => {
     const result = req.body.number1 + req.body.number2
     res.status(204).json({
         result: result// ./fop -fo examples/fo/tables/headfoot.fo -pdf ./header-table.pdf 
     })
-    console.log("====>>>")
 
     exec("./trialscript 'headfoot.fo' '../../opt/fop-my-app/pdfs/header-1.pdf'", (error, stdout, stderr) => {
         if (error) {
@@ -35,7 +40,7 @@ app.post('/sum',async  (req, res) => {
         console.log(`stdout: ${stdout}`);
     });
     const s3 = new AWS.S3({
-        region:'us-east-1',
+        region: 'us-east-1',
     })
     const filePdf = './pdfs/header-1.pdf'
     const uploadImageResult = await fetch(filePdf)
@@ -46,26 +51,7 @@ app.post('/sum',async  (req, res) => {
         Key: req.files[0].originalFilename,
         Body: blob,
     }).promise()
-    console.log(uploadedImage.Location)
-    console.log("0000>>>>", result)
 })
-
-// app.get('/get-pdf',(req,res)=>{
-
-//     let reporter = new Reporter().use(require('./')())
-
-//     reporter.init()
-//     jsreport.init()
-
-//     const request = {
-//         template: { content: fs.readFileSync(path.join(__dirname, '/hello.fo')).toString(), recipe: 'fop-pdf', engine: 'none' }
-//       }
-//       console.log("--->>>>",request)
-//       return reporter.render(request).then(function (response) {
-//         response.content.toString().should.containEql('%PDF')
-//       })
-
-// })
 
 app.listen(port, () => {
     console.log('listening to port ', port)
---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import { exec } from "child_process";
+import AWS from 'aws-sdk';
+import convert from 'xml-js';
+import fs from 'fs';
+import bodyParser from "body-parser";
+import path from "path";
+import { writeFileSync } from 'fs';
+import { rando } from '@nastyox/rando.js';
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.json())
+
+app.post( "/export-to-pdf", ( req, res ) => {
+    const options = {compact: true, ignoreComment: true, spaces: 4};
+    const result = convert.json2xml(req.body, options);
+    console.log("fop file")
+    const fileName = `${rando(100000)}-pdf`
+    writeFileSync(`../fops/${fileName}.fo`, result);
+    console.log("file name: ",fileName)
+    exec(`fopScript '../../opt/fop-my-app/fops/${fileName}.fo' '../../opt/fop-my-app/pdfs/${fileName}.pdf'`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+} );
+
+app.listen( port, () => {
+    console.log( `server started at port ${ port }` );
+} );
